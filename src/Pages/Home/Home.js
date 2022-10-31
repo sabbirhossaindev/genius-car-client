315d9bfe9277fb72ad9d008c8fe0c14bdafbcca0
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-31 17:53:54 +0600

    home done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import About from './About/About';
+import Banner from './Banner/Banner';
+
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Banner></Banner>
+            <About></About>
         </div>
     );
 };
