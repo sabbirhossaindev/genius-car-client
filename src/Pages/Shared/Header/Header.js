c98d01d9d0821339e601fa69852a30aa59a11ea6
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-02 21:35:05 +0600

    home done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.svg';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
-
-const menuItems = <>
-        <li className='font-semibold'><Link to='/'>Home</Link></li>
-        <li className='font-semibold'><Link to='/login'>Login</Link></li>
-</>
-    
 const Header = () => {
+    const {user} = useContext(AuthContext);
+    const menuItems = <>
+        <li className='font-semibold'><Link to='/'>Home</Link></li>
+        {
+            user?.email ? 
+            <>
+            <li className='font-semibold'><Link to='/orders'>Orders</Link></li>
+            </>
+            :
+            <li className='font-semibold'><Link to='/login'>Login</Link></li>
+        }
+    </>
+
     return (
         <div className="navbar h-20 mb-12 pt-12 bg-base-100">
             <div className="navbar-start">
@@ -30,7 +38,7 @@ const Header = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-            <button className="btn btn-outline btn-warning">Appointment</button>
+                <button className="btn btn-outline btn-warning">Appointment</button>
             </div>
         </div>
     );
