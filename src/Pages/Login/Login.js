c20a042b756a9b91fff9d11e08e5122ad395809e
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-04 20:52:53 +0600

    token done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -20,10 +20,29 @@ const Login = () => {
         login(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user);
-                navigate(from, {replace: true})
+                const currentUser = {
+                    email: user.email
+                }
+
+                console.log(currentUser);
+
+                // get jwt token
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })       
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        // local Storage is the easiest but not the best place
+                        localStorage.setItem('Genius-token', data.token)
+                        navigate(from, {replace: true})
+                    })
             })
-            .then(error => console.error(error));
+            .catch(error => console.error(error));
     }
 
     return (
